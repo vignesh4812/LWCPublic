3d47930f9b4af65f0e686a62297c4173f9f230c4
vignesh4812 <vignesh4812@gmail.com>
2020-03-04 14:03:54 +0530

    Delete helloWorldLWC.js

--- helloWorldLWC.js
+++
@@ -1,8 +0,0 @@
-import { LightningElement} from 'lwc';
-
-export default class HelloWorldLWC extends LightningElement {
-    Name = 'Name Tested Okay!!';
-    displayTime() {
-      this.Name = new Date();;
-    }    
-}
